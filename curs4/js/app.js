a4b284ca00d95cfed73fd86ed52b7bd2357565d1
aadiaconitei <aadiaconitei@yahoo.com>
2024-02-15 19:40:55 +0200

    curs4

--- curs4/js/app.js
+++ curs4/js/app.js
@@ -1,14 +1,9 @@
-function creatediv(n){
-    for (var h = 0; h < n; h++) {
-        var div = document.createElement('div');    
-        // div.innerHTML = h+1;
-        div.className = 'box';
-        document.getElementById("wrapper").appendChild(div);
-        
-    }
-    
+function creatediv(n) {
+  for (var h = 0; h < n; h++) {
+    var div = document.createElement("div");
+    // div.innerHTML = h+1;
+    div.className = "box";
+    document.getElementById("wrapper").appendChild(div);
+  }
 }
 creatediv(72);
-
-
-
