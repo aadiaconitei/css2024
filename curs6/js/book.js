6f585fb1e9b443b2ba5ec486cc988c4af4f32475
aadiaconitei <aadiaconitei@yahoo.com>
2024-03-06 18:32:33 +0200

    curs7

--- curs6/js/book.js
+++ curs6/js/book.js
@@ -1,16 +1,23 @@
-function Book(title,author,page){
-    this.title = title;
-    this.author = author;
-    this.page = page;
-    this.toString = toString;
+function Book(title, author, page) {
+  this.title = title;
+  this.author = author;
+  this.page = page;
+  this.toString = toString;
+  //   this.inchidCartea = inchidCartea;
 }
 
-function toString(){
-    return this.title + ' de ' + this.author + ', si are '+ this.page + ' pagini.'
+function toString() {
+  return (
+    this.title + " de " + this.author + ", si are " + this.page + " pagini."
+  );
 }
 
-function citeste(){
-    return "Acum citesc o carte";
+function inchidCartea() {
+  return "Fac pauza";
+}
+
+function citeste() {
+  return "Acum citesc o carte";
 }
 // export {Book};
-export {Book, citeste};
+export { Book, citeste, inchidCartea };
--- curs6/js/main.js
+++ curs6/js/main.js
@@ -1,10 +1,13 @@
-import {Book, citeste} from './book.js';
-
+import { Book, citeste, inchidCartea } from "./book.js";
 
 //  import * from " demo.js"
 //  import { func1, obj2 } from "demo.js"
 
-
-let carte = new Book('Poezii','Mihai Eminescu', 250);
- console.log(carte.toString());
- console.log(citeste());
+function citeste() {
+  return " Nu mai citesc!";
+}
+let carte = new Book("Poezii", "Mihai Eminescu", 250);
+console.log(carte);
+console.log(carte.toString());
+console.log(citeste());
+console.log(inchidCartea());
